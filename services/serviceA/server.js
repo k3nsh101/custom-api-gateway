fd66b4a638f982a9b328002100cb6175bebb363b
kenshi <samuduchanuka@gmail.com>
2025-09-13 20:29:17 +0530

    Remove unnecessary logging

--- services/serviceA/server.js
+++ services/serviceA/server.js
@@ -3,13 +3,10 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  console.log(req.headers);
   res.send("Hello from service A");
 });
 
 app.get("/:id", (req, res) => {
-  console.log(req.headers);
-  console.log(req.path);
   res.send("Hello from service A");
 });
 
